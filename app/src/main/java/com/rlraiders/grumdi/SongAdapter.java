20c94c25d4baf7e84108356e6681915cf206e4e4
Ryan McDonough <ryan@wearehathway.com>
2015-03-07 00:52:40 -0800

    adding adapter to map songs to list view

---
+++ app/src/main/java/com/rlraiders/grumdi/SongAdapter.java
@@ -0,0 +1,45 @@
+package com.rlraiders.grumdi;
+
+import android.widget.BaseAdapter;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by ryanmcdonough on 3/7/15.
+ */
+public class SongAdapter extends BaseAdapter {
+
+    private ArrayList<Song> songs;
+    private LayoutInflater songInf;
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+    }
+}
